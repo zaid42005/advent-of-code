5332d0a180aff6dcf6275a453bc93a93323874a4
zaid42005 <zaid.42005@gmail.com>
2023-12-21 23:32:01 -0500

    wtf run part 2 man

--- day-01/part2/part2.py
+++ day-01/part2/part2.py
@@ -1,13 +1,20 @@
 def main(input):
     lines = input.splitlines()
     total1 = 0
+    total2 = 0
     for line in lines:
-        print(line)
         digits = [char for char in line if char.isnumeric()] #only add the char if its numeric, list comprehension yum
-        print (digits)
-        total1 += int(digits[0] + digits[-1]) #first and last in each digits list
-        print (total1)
+        if digits:
+            total1 += int(digits[0] + digits[-1]) #first and last in each digits list
+        digits = [char for char in translate(line) if char.isnumeric()]
+        total2 += int(digits[0] + digits[-1])
+        
     
-    return 
+    return total1,total2
 
-main(open("input.txt","r").read())
+def translate(line):
+    for num, name in enumerate(digit_names):
+        print (num,name)
+
+digit_names = ["one","two","three","four","five","six","seven","eight","nine"]
+main(open("test1.txt","r").read())
