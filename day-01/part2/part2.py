db60cc1cae5d373ce1a6947dccd010ce3d771c6b
zaid42005 <zaid.42005@gmail.com>
2023-12-21 23:16:51 -0500

    cleaned up file structure

---
+++ day-01/part2/part2.py
@@ -0,0 +1,13 @@
+def main(input):
+    lines = input.splitlines()
+    total1 = 0
+    for line in lines:
+        print(line)
+        digits = [char for char in line if char.isnumeric()] #only add the char if its numeric, list comprehension yum
+        print (digits)
+        total1 += int(digits[0] + digits[-1]) #first and last in each digits list
+        print (total1)
+    
+    return 
+
+main(open("input.txt","r").read())
